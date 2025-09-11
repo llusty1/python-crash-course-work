8476ba9231220b0d2f31512e1c8cdde09ed6cda8
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-11 00:11:00 -0500

    Commit

---
+++ name.py
@@ -0,0 +1,15 @@
+#This is the name exercise in Python Crash Course
+
+name = "ada lovelace"
+#These are the various methods in this exercise
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#Now I will learn to concatenate strings
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
