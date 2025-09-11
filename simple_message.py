165d14f1854b09d3f1c99dc0063bc111e92f74cb
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-10 23:14:00 -0500

    Commit

---
+++ simple_message.py
@@ -0,0 +1,9 @@
+#This is the simple message exercise in the python crash course second chapter
+
+message = 'This is a simple message.'
+print(message)
+
+#We added another message to print
+
+message = 'This is another message.'
+print(message)
