6376e29c2c324d7e91c0808df2c4f50dfcf91aa2
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-17 12:29:57 -0500

    Commit

--- simple_message.py
+++ simple_message.py
@@ -1,4 +1,5 @@
 #This is the simple message exercise in the python crash course second chapter
+#This exercise is focused on variables, in particular defining the message variable
 
 message = 'This is a simple message.'
 print(message)
