862658934d59bbec65b2324412b8ece30556360a
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-19 20:06:40 -0500

    Commit

--- numbers_chapter_2.py
+++ numbers_chapter_2.py
@@ -1,1 +1,5 @@
-# This file will be about numbers. 
+# You can add + subtract - multiply * and divide / integers in Python
+# In a terminal session you can do these things
+# Python supports order of operations too
+# Example 2+3*4 = 14  (2+3)*4 = 20
+# Floats are numbers with decimal points in them and Python does a nice job with those as well. 
