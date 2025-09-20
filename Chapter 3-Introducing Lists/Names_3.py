d0694d199722063b74fdc6cfbf7ea4b66a34f8e5
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-20 18:19:52 -0500

    Commit

---
+++ Chapter 3-Introducing Lists/Names_3.py
@@ -0,0 +1,15 @@
+# Store the names of a few of your friends in a list called names. 
+# Print each person's name by accessing each element in the list, one at a time.
+# I will use some familiar names 
+names = ['harry potter', 'ron weasley', 'hermoine granger']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+
+# Now we will create a greeting using the names above
+names = ['harry potter', 'ron weasley', 'hermoine granger']
+print(f"Hello, {names[0].title()}... Welcome to Python!")
+print(f"Hello, {names[1].title()}... Welcome to Python!")
+print(f"Hello, {names[2].title()}... Welcome to Python!")
+# This one took awhile, and I used other code for reference.
+# Remember to keep trying to figuring things out.
