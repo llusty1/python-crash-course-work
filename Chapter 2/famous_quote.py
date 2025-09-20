aca297335cfd84a53280c0cff427867631534000
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-19 19:21:12 -0500

    Commit

---
+++ Chapter 2/famous_quote.py
@@ -0,0 +1,3 @@
+#This exercise is to practice the use of double-quotes so that you can use single quotes inside of a variable.
+quote = "Albert Einstein once said, 'The person who never made a mistake never tried anything new.'"
+print(quote)
--- strings.py
+++
@@ -1,3 +0,0 @@
-#This file is strings focused
-#A string is series of characters
-#Anything inside quotes is considered a string in Python, and you can use single or double quotes around your strings
