1541637741374c6b1cafa08332d7568456524cc5
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-19 19:39:08 -0500

    Commit

---
+++ Chapter 2/famous_person.py
@@ -0,0 +1,7 @@
+#This exercise is meant to list two variables famous_person and message then we print it out!
+famous_person = "Albert Einstein"
+message = "once said, 'The person who never made a mistake never tried anything new."
+print(famous_person, message) # I figured out that using a comma here would work. 
+# Another way of doing this would be to use the f-string
+print(f"{famous_person},{message}")
+# Both ways produce similar results. 
