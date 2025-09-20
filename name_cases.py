5c7ab4d266de7c59a1d29208d690cd171ddbab3e
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-19 19:03:21 -0500

    Commit

---
+++ name_cases.py
@@ -0,0 +1,5 @@
+#I will store a person's name in a variable and then print the uppercase, lowercase, and titlecase versions of it.
+name = "ben venidos" #jajajaja no mames guey!
+print(name.upper())
+print(name.lower())
+print(name.title())
