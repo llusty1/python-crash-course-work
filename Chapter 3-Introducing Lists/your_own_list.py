3c0a3e683874ffa02a029b317e120863e12309eb
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-20 18:28:25 -0500

    Commit

---
+++ Chapter 3-Introducing Lists/your_own_list.py
@@ -0,0 +1,5 @@
+# Here we will create another list and then a statement 
+# We will focus on motorcycle brands
+motorcycle = ['harley davidson panhead', 'honda', 'bmw', 'indian']
+message = "I would like to own a " + motorcycle[0].title() + " someday."
+print(message)
