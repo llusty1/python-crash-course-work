8e12a017fe59ff2e62f5861bfae2025469095b38
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-27 12:41:17 -0500

    Commit

--- Chapter 3-Introducing Lists/guest_list.py
+++ Chapter 3-Introducing Lists/guest_list.py
@@ -1,3 +1,6 @@
 # This exercise is to create a list of at least 3 people you'd like to invite to dinner.
 list = ["Michael Jordan", "Shaquille O'Neal", "Charles Barkley",]
 print(list)
+
+# Now let's create a message to each guest that you'd like to invite.
+message = "Dear Mr. " +
