8802e6e6747db96f199db05ebc7674ddc52a384f
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-27 15:20:48 -0500

    Commit

--- Chapter 3-Introducing Lists/guest_list.py
+++ Chapter 3-Introducing Lists/guest_list.py
@@ -1,6 +1,11 @@
 # This exercise is to create a list of at least 3 people you'd like to invite to dinner.
 list = ["Michael Jordan", "Shaquille O'Neal", "Charles Barkley",]
 print(list)
-
 # Now let's create a message to each guest that you'd like to invite.
-message = "Dear Mr. " +
+message = "Dear Mr. " + list[0] + " we humbly invite you to dinner. "
+print(message)
+message = "Dear Mr. " + list[1] + " we humbly invite you to dinner. "
+print(message)
+message = "Dear Mr. " + list[2] + " we humbly invite you to dinner. " 
+print(message)
+# This took me a minute, I will learn if there is a more efficient way of doing this. 
