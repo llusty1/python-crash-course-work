f7de98a1b44cb5679643d6db19361741ba898a00
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-29 22:06:54 -0500

    Commit

---
+++ Chapter 4-Working With Lists/animals.py
@@ -0,0 +1,5 @@
+# A list of animals
+animals = ["Cats", "Birds", "Dogs"]
+for animal in animals:
+    print(animal + " make wonderful pets.")
+print("Many households would call these animals members of their families.")
