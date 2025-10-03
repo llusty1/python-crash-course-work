b5616c6604513391a5907fd7f47e9e6757f21c86
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-02 23:04:14 -0500

    Commit

--- Chapter 4-Working With Lists/numbers.py
+++ Chapter 4-Working With Lists/numbers.py
@@ -2,3 +2,11 @@
 # Let's use the range() function
 for value in range(1, 5):
     print(value)
+
+# Since Python is one off, let's print the range() function for 1-5
+for value in range(1, 6):
+    print(value)
+
+# Using range() to make a list of numbers 
+numbers = list(range(1,6))
+print(numbers)
