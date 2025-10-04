ff23a2072d75403c37e475df61e23c7bd2d57e81
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 14:44:41 -0500

    Commit

--- Chapter 4-Working With Lists/cubes.py
+++ Chapter 4-Working With Lists/cubes.py
@@ -5,6 +5,6 @@ for value in range(1,11): # Now we create the range
     cube = value**3 
     cubes.append(cube)
 print(cubes)
-# Here is a list comprehension
+# Here is a list comprehension to show the values of the first 10 cubes
 cubes = [value**3 for value in range(1,11)] # Notice there are two * as this problem calls for cubing the numbers. 
 print(cubes)
