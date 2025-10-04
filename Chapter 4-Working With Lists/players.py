ba71c27751a67bbf66b9498bd2fb2aaa30ad1c07
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 15:11:51 -0500

    Commit

--- Chapter 4-Working With Lists/players.py
+++ Chapter 4-Working With Lists/players.py
@@ -1,3 +1,8 @@
 # Here we will create a list of players and then slice the list using their index position.
 players = ["Agnes", "Agatha", "Jermaine", "Jack", "Biz" ] # Just having a friend couldn't be no crime
 print(players[0:4]) # Cause I've got friends and that's a fact like:
+print(players[1:4])
+print(players[:4])
+print(players)
+print(players[2:])
+print(players[-3:])
