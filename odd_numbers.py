af92f9bdb6f278d1cdb8e8082ad830f6270654c9
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 14:23:44 -0500

    Commit

---
+++ odd_numbers.py
@@ -0,0 +1,4 @@
+# Here we will make a list of numbers 1,21 using the range function and print out odd numbers using a for loop
+odd_numbers = list(range(1,21,2))
+for number in odd_numbers:
+    print(number)
