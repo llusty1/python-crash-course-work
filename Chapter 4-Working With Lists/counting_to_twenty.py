09cbddd78b522d4e6ddecc7024e083c41be37c4e
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 13:38:51 -0500

    Commit

---
+++ Chapter 4-Working With Lists/counting_to_twenty.py
@@ -0,0 +1,3 @@
+# This exercise is about counting to the number twenty using a list.
+count = list(range(1,21)) # Here I named a variable count = a list wrapped around a range of numbers 1,21
+print(count) # Here we print out the list created above and should get the range of numbers from 1,21
