e35a7d9369a622c8a8cea72cb8579fd42cc7f20c
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 14:43:15 -0500

    Commit

--- Chapter 4-Working With Lists/cubes.py
+++ Chapter 4-Working With Lists/cubes.py
@@ -1,3 +1,10 @@
 # Here we will cube a list of numbers from 1-10
+# We start with an empty list
+cubes = []
+for value in range(1,11): # Now we create the range
+    cube = value**3 
+    cubes.append(cube)
+print(cubes)
+# Here is a list comprehension
 cubes = [value**3 for value in range(1,11)] # Notice there are two * as this problem calls for cubing the numbers. 
 print(cubes)
