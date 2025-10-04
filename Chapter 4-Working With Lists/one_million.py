c5e417cdf736d438a1a9ede2e7cb3861e4acb8ff
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 13:49:41 -0500

    Commit

---
+++ Chapter 4-Working With Lists/one_million.py
@@ -0,0 +1,8 @@
+# Here we will create a list with a range of a million = 1,1000001
+million = list(range(0,1000001))
+print(million)
+
+# Here we will print the min, max, and the sum of the list
+print(min(million))
+print(max(million))
+print(sum(million))
