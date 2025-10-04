62e184d0a011f4546f7b70ff00957de9c07cd937
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-04 10:56:26 -0500

    Commit

--- Chapter 4-Working With Lists/squares.py
+++ Chapter 4-Working With Lists/squares.py
@@ -2,5 +2,5 @@
 squares = [] # We start with an empty list
 for value in range(1,11): # Next we write a for loop using the range() function for 1-11
     square = value**2 
-    squares.append(square) 
+    squares.append(square)
 print(squares)
