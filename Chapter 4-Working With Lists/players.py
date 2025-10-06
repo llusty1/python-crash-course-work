daa1f2fbe4f8991cb615d74c994ea2514750dd08
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-06 08:09:20 -0500

    Commit

--- Chapter 4-Working With Lists/players.py
+++ Chapter 4-Working With Lists/players.py
@@ -6,3 +6,7 @@ print(players[:4])
 print(players)
 print(players[2:])
 print(players[-3:])
+# Now let's print out the first three players using a for loop. 
+print("\nHere are the first three players on my team:")
+for player in players[:3]: # Instead of looping through the entire list Python loops through only the first three names.
+    print(player.title())
