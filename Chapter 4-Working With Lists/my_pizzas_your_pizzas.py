59b2b4d253ef5c08f935df87bbc489ee3bfdc717
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-09 23:27:07 -0500

    Commit

---
+++ Chapter 4-Working With Lists/my_pizzas_your_pizzas.py
@@ -0,0 +1,8 @@
+my_pizzas = ["Margherita", "Napoli", "Romana"]
+friend_pizzas = my_pizzas[:]
+my_pizzas.append("Sicilian")
+friend_pizzas.append("Deep Dish")
+print("My favorite pizzas are:")
+print(my_pizzas)
+print("\nMy friend's favorite pizzas are:")
+print(friend_pizzas)
