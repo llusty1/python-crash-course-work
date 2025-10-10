beeae673fa6034d29bf8f75a9867c1f93e972fec
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-10-09 22:22:46 -0500

    Commit

---
+++ Chapter 4-Working With Lists/slices.py
@@ -0,0 +1,7 @@
+print("The first three items in this list are:")
+my_list = ["Twelve drummers drumming", "Eleven pipers piping", "Ten lords a-leaping", "Nine ladies dancing",
+           "Eight maids a-milking", "Seven swans a-swimming", "Six geese a-laying", "Five golden rings!",
+           "Four calling birds", "Three french hens", "Two turtle doves", "and A partridge in a pear tree."]
+print(my_list[:3]) # This here prints out the first three items. Notice we are listing the items by their index position. 
+print(my_list[3:8])# This prints out the middle of the list. 
+print(my_list[8:]) # And finally we print out the rest of the list. 
