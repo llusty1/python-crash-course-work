5346c73d6473d654930c9e5af27dd2376354af47
llusty1 <lawrencelusty@proton.me>
2025-10-12 13:21:01 -0500

    Commit

---
+++ Chapter 5-If Statements/cars.py
@@ -0,0 +1,8 @@
+# The first example of this chapter is familiar code from previous lessons:
+cars = ["audi", "bmw","subaru", "toyota"]
+
+for car in cars:
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())    
