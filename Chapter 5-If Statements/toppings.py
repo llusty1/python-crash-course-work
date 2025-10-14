855288d39d58ca9653c86ce595fe8bdffdccf0de
llusty1 <lawrencelusty@proton.me>
2025-10-14 18:45:26 -0500

    Commit

---
+++ Chapter 5-If Statements/toppings.py
@@ -0,0 +1,5 @@
+# Here we will check for Inequality
+requested_topping = "mushrooms" # Here is the request
+
+if requested_topping != "anchovies": # If is conditional != means does not equal
+    print("Hold the anchovies!")
