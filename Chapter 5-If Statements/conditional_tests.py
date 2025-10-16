815b42f7852898d53a8f0d802d2c995da77f3ff5
llusty1 <lawrencelusty@proton.me>
2025-10-15 21:38:53 -0500

    Commit

---
+++ Chapter 5-If Statements/conditional_tests.py
@@ -0,0 +1,7 @@
+# Here we will write conditional tests (if statements)
+car = "subaru"
+print("Is car == 'subaru'? I predict true")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict false")
+print(car == 'audi')
