f141e7c99fa223aeacc0aede3cc110a4ad05a2ed
llusty1 <lawrencelusty@proton.me>
2025-10-15 22:18:05 -0500

    Commit

--- Chapter 5-If Statements/conditional_tests.py
+++ Chapter 5-If Statements/conditional_tests.py
@@ -5,3 +5,31 @@ print(car == 'subaru')
 
 print("\nIs car == 'audi'? I predict false")
 print(car == 'audi')
+
+amp = "Orange"
+print("\nIs amp == 'Orange'? This is true")
+print(amp == "Orange")
+
+print("\nIs amp == 'Marshall'? This is false")
+print(amp == "Marshall")
+
+guitar = "Les Paul"
+print("\nIs guitar == 'Les Paul'? This is true")
+print(guitar == "Les Paul")
+
+print("\nIs guitar == 'SG'? This is false")
+print(guitar == "SG")
+
+work = "Massage Therapist"
+print("\nI work in == 'Massage Therapist'? This is true")
+print(work == "Massage Therapist")
+
+print("\nI work in == 'Help Desk'? This is false")
+print(work == "Help Desk")
+
+study = "Network Engineer"
+print("\nDoes study == 'Network Engineer'? This is true")
+print(study == "Network Engineer")
+
+print("\nDoes study == 'Psychology'? This is false")
+print(study == "Psychology")
