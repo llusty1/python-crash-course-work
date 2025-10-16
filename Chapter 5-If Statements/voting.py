251f693820aceaa28a044059b698a541d638fcb6
llusty1 <lawrencelusty@proton.me>
2025-10-15 22:53:50 -0500

    Commit

---
+++ Chapter 5-If Statements/voting.py
@@ -0,0 +1,10 @@
+# Simple if statement
+age = 17
+if age >= 18: # This should read if age is greater than or equal to:
+    print("You can vote cabron!")
+    print("Are you registered to vote pendejo?")
+    print("Exercise your pinche rights and do it carnale!")
+    print("Otra Vez, La Raza is counting on you Holmes.")
+else: # Unless the condition from above is met: 
+    print("Sorry young buck come back next time.")
+    print("Register to vote the second you turn 18.")
