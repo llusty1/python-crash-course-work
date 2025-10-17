8f1a95f4982ea90242219d3ad7ec3be6c35128df
llusty1 <lawrencelusty@proton.me>
2025-10-16 22:57:23 -0500

    Commit

---
+++ Chapter 5-If Statements/amusement_park.py
@@ -0,0 +1,11 @@
+# The if-elif-else Chain
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+
+elif age < 18:
+    print("Your admission cost is $5.")
+
+else:
+    print("Your admission cost is $10.")
