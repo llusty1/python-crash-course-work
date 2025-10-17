ea085d177049971331704376969f4abca13da741
llusty1 <lawrencelusty@proton.me>
2025-10-16 23:04:46 -0500

    Commit

--- Chapter 5-If Statements/amusement_park.py
+++ Chapter 5-If Statements/amusement_park.py
@@ -9,3 +9,17 @@ elif age < 18:
 
 else:
     print("Your admission cost is $10.")
+
+# Here we will insert the prices inside the chain.
+age = 12
+
+if age < 4:
+    price = 0
+
+elif age < 18:
+    price = 5
+
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
