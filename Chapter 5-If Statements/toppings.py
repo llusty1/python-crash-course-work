795e31c9733336de28d5abf36563b661363f9928
llusty1 <lawrencelusty@proton.me>
2025-10-23 09:21:46 -0500

    Commit

--- Chapter 5-If Statements/toppings.py
+++ Chapter 5-If Statements/toppings.py
@@ -3,3 +3,15 @@ requested_topping = "mushrooms" # Here is the request
 
 if requested_topping != "anchovies": # If is conditional != means does not equal
     print("Hold the anchovies!")
+
+# Here we will modify the requests
+requested_toppings = ["mushrooms", "extra cheese"]
+
+if "mushrooms" in requested_toppings:
+    print("Adding mushrooms.")
+if "pepperoni" in requested_toppings:
+    print("Adding pepperoni.")
+if "extra cheese" in requested_toppings:
+    print("Adding extra cheese.")
+    
+print("\nFinished making your pizza!")
