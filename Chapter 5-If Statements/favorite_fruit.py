65a40f028117300776a815606404b79597d9a1e9
llusty1 <lawrencelusty@proton.me>
2025-10-26 18:53:50 -0500

    Commit

---
+++ Chapter 5-If Statements/favorite_fruit.py
@@ -0,0 +1,12 @@
+# 5.7 Favorite Fruit Exercise
+favorite_fruits = ["cherries", "blueberries", "bannanas"]
+if "cherries" in favorite_fruits:
+    print("You really like cherries!")
+if "blueberries" in favorite_fruits:
+    print("You really like blueberries!")
+if "bannanas" in favorite_fruits:
+    print("You really like bannanas!")
+if "oranges" in favorite_fruits:
+    print("You really like oranges!")
+if "apples" in favorite_fruits:
+    print("You really like apples!")
