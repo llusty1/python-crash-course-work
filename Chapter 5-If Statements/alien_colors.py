ac21003007dcaf08b22802efec1c6787bee25f1b
llusty1 <lawrencelusty@proton.me>
2025-10-26 18:00:01 -0500

    Commit

--- Chapter 5-If Statements/alien_colors.py
+++ Chapter 5-If Statements/alien_colors.py
@@ -1,10 +1,10 @@
-# Alien colors exercise
+# Alien colors exercise #1
 alien_colors = ["green", "yellow", "red"]
 if "green" in alien_colors:
     print("You have earned 5 points!")
 elif "blue" in alien_colors:
     print("You have earned 10 point!")
-elif "yellow" in alien_colors: # Even though yellow is in the list it doesn't work because of elif rules.
+elif "yellow" in alien_colors: # Even though yellow is in the list it doesn't run because the first if statement passed the conditional so the loop stopped running
     print("Yellow is mellow.")
 
 print("Lil' Flip says game over.")
@@ -19,3 +19,62 @@ if "yellow" in alien_colors: # If statement works in this loop where elif fails.
     print("Yellow is mello.")
 
 print("Lil' Flip says game over.")
+
+# Alien colors exercise #2
+alien_colors = ["green","yellow", "red"]
+if "green" in alien_colors:
+    print("\nYou have earned 5 points!")
+else:
+    print("\nYou have earned 10 points!")
+
+alien_colors = ["green", "blue", "red"]
+if "green" in alien_colors:
+    print("\nYou have earned 5 points Green!")
+if "blue" in alien_colors:
+    print("You have earned 10 points Blue!")
+if "red" in alien_colors:
+    print("You have earned 20 points Red!")
+
+# Alien colors exercise #3
+alien_colors = ["green", "blue", "red"]
+if "green" in alien_colors:
+    print("\nYou have earned 5 points Green!")
+if "blue" in alien_colors:
+    print("You have earned 10 points Blue!")
+if "red" in alien_colors:
+    print("You have earned 15 points Red!")
+
+alien_colors = ["green", "blue", "red"]
+if "green" in alien_colors:
+    print("\nYou have earned 5 points Green!")
+elif "blue" in alien_colors:
+    print("You have earned 15 points Blue!")
+elif "red" in alien_colors:
+    print("You have earned 15 points Red!")
+else:
+    print("You have zero points!")
+
+color = "green"
+if "green" in color:
+    print("\nYou have earned 5 points Green!")
+if "blue" in color:
+    print("\nYou have earned 10 points Blue!")
+if "red" in color:
+    print("\nYou have earned 15 points Red!")
+
+color = "blue"
+if "green" in color:
+    print("You have earned 5 points Green!")
+if "blue" in color:
+    print("You have earned 10 points Blue!")
+if "red" in color:
+    print("You have earned 15 points Red!")
+
+color = "red"
+if "red" in color:
+    print("You have earned 5 points Green!")
+if "blue" in color:
+    print("You have earned 10 points Blue!")
+if "red" in color:
+    print("You have earned 15 points Red!")
+print("Thanks for playing!")
