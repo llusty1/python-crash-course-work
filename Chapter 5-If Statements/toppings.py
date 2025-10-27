43022ff8b7853e452ca8d3aa06772e42f911cf70
llusty1 <lawrencelusty@proton.me>
2025-10-27 10:52:07 -0500

    Commit

--- Chapter 5-If Statements/toppings.py
+++ Chapter 5-If Statements/toppings.py
@@ -14,4 +14,13 @@ if "pepperoni" in requested_toppings:
 if "extra cheese" in requested_toppings:
     print("Adding extra cheese.")
     
-print("\nFinished making your pizza!")
+print("\nFinished making your pizza!\n")
+
+# Using if Statements with Lists example later in the chapter.
+requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
+for requested_topping in requested_toppings:
+    if requested_topping == "green peppers":
+        print("Sorry we are out of green peppers at this time.")
+    else:
+        print("Adding " + requested_topping + ".")
+print("\nFinished making your pizza!\n")
