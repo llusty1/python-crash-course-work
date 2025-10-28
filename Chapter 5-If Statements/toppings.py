62beb011cca514854cde698fe55814a80222bf1a
llusty1 <lawrencelusty@proton.me>
2025-10-28 10:57:49 -0500

    Commit

--- Chapter 5-If Statements/toppings.py
+++ Chapter 5-If Statements/toppings.py
@@ -14,7 +14,7 @@ if "pepperoni" in requested_toppings:
 if "extra cheese" in requested_toppings:
     print("Adding extra cheese.")
     
-print("\nFinished making your pizza!\n")
+print("Finished making your pizza!\n")
 
 # Using if Statements with Lists example later in the chapter.
 requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
@@ -23,4 +23,25 @@ for requested_topping in requested_toppings:
         print("Sorry we are out of green peppers at this time.")
     else:
         print("Adding " + requested_topping + ".")
-print("\nFinished making your pizza!\n")
+print("Finished making your pizza!\n")
+
+# Checking that a list is not empty
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?\n")
+
+# Using multiple lists
+available_toppings = ["mushrooms", "olives", "green peppers", "pepperoni", "pineapple", "extra cheese"]
+requested_toppings = ["mushrooms", "fries", "extra cheese"]
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry we don't have " + requested_topping + ".")
+print("Finished making your pizza!")
