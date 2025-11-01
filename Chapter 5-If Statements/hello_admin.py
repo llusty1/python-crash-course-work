c66c01f8ad897f174df72a41e91b47914b668b1d
llusty1 <lawrencelusty@proton.me>
2025-11-01 15:02:19 -0500

    Commit

--- Chapter 5-If Statements/hello_admin.py
+++ Chapter 5-If Statements/hello_admin.py
@@ -6,3 +6,11 @@ for user in user_names:
         print("Hello " + user + ", would you like a status report?\n")
     else: 
         print("Hello, " + user + " thank you for logging in.")
+
+# Now we will ad to our code an empty list:
+user_names = []
+
+for user in user_names:
+    print("Hello, " + user + ", would you like a status report? ")
+else:
+    print("\nWe've got to find some users!")
