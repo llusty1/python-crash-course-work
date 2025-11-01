653757ee5fc4b4274d6ac0926b70a66f1f2eb4bf
llusty1 <lawrencelusty@proton.me>
2025-11-01 14:41:11 -0500

    Commit

---
+++ Chapter 5-If Statements/hello_admin.py
@@ -0,0 +1,8 @@
+# Hello Admin Exercise
+user_names = ["Admin", "Agness", "Agatha", "Germaine", "Jacq"]
+
+for user in user_names:
+    if user == "Admin":
+        print("Hello " + user + ", would you like a status report?\n")
+    else: 
+        print("Hello, " + user + " thank you for logging in.")
