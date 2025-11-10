648b587620ddeb848dcb013aef8a5daf1a1e2424
llusty1 <lawrencelusty@proton.me>
2025-11-09 22:30:36 -0600

    Commit

--- Chapter 6-Dictionaries/alien.py
+++ Chapter 6-Dictionaries/alien.py
@@ -2,3 +2,12 @@ alien_0 = {"color": "green", "points": 5}
 
 print(alien_0["color"])
 print(alien_0["points"])
+
+# New code in the chapter
+alien_0 = {"color": "green", "points":5}
+print(alien_0)
+
+#More code
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
