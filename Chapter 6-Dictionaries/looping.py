43ff9bca50fa58857fa39060c33050e9a6e5e353
llusty1 <lawrencelusty@proton.me>
2025-12-07 13:02:55 -0600

    Commit

---
+++ Chapter 6-Dictionaries/looping.py
@@ -0,0 +1,10 @@
+# Here we will create dictionaries to learn how to loop through them
+user_0 = {
+    "username": "efermi",
+    "first": "enrico",
+    "last": "fermi",
+}
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
