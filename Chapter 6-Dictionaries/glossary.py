903e4dd6fd9da1cb3c56433d63475dfa56f56b22
llusty1 <lawrencelusty@proton.me>
2025-12-07 12:05:23 -0600

    Commit

--- Chapter 6-Dictionaries/glossary.py
+++ Chapter 6-Dictionaries/glossary.py
@@ -1,1 +1,12 @@
 # Here is the exercise for a glossary in the chapter
+python_words = {"print": "A buit-in function used to output messages or values to the console or screen",
+                "string": "A fundemental data type used to represent text characters, enclosed in single quotes ('') or double quotes (\"\").",
+                "variable": "A named container that stores data values in memory, allowing them to be referenced and manipulated throughout a program.",
+                "list": "A collection data type used to store an ordered, changeable sequence of items. Lists are defined using square brackets ([]).",
+                "dictionary": "A collection data type used to store data values in key-value pairs. Dictionaries are unordered, changeable, and indexed by keys (not numbers), defined using curly braces ({})."
+                }
+print("Print: " + str(python_words["print"]))
+print("String: " + str(python_words["string"]))
+print("Variable: " + str(python_words["variable"]))
+print("List: " + str(python_words["list"]))
+print("Dictionary: " + str(python_words["dictionary"]))
