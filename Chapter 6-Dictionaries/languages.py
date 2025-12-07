e5da62db5135bdabacc1e8a9f70b0a4fca9a6415
llusty1 <lawrencelusty@proton.me>
2025-12-07 11:06:04 -0600

    Commit

---
+++ Chapter 6-Dictionaries/languages.py
@@ -0,0 +1,11 @@
+# Here is a dictionary a Similar Objects
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "ruby",
+    "phil": "python",
+    }
+
+print("Sarah's favorite language is " + 
+      favorite_languages["sarah"].title() + 
+      ".")
