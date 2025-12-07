e2450448c9875ee985ea34b676ad9f86ba62202b
llusty1 <lawrencelusty@proton.me>
2025-12-07 11:31:46 -0600

    Commit

---
+++ Chapter 6-Dictionaries/favorite_numbers.py
@@ -0,0 +1,11 @@
+# This is the favorite number exercise
+favorite_numbers = {"Dak":4,
+                    "George":3,
+                    "CeeDee":88,
+                    "Javonte":33,
+                    "Jake":47,}
+print("Dak Prescott's number is: " + str(favorite_numbers["Dak"]))
+print("George Picken's number is: " + str(favorite_numbers["George"]))
+print("CeeDee Lamb's number is: " + str(favorite_numbers["CeeDee"]))
+print("Javonte William's number is: " +str(favorite_numbers["Javonte"]))
+print("Jake Ferguson's bumer is: " + str(favorite_numbers["Jake"]))
