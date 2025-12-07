636a6b80fc11c032c679679556337b56e6a92aac
llusty1 <lawrencelusty@proton.me>
2025-12-07 09:33:58 -0600

    Commit

--- Chapter 6-Dictionaries/alien.py
+++ Chapter 6-Dictionaries/alien.py
@@ -29,3 +29,27 @@ print("The alien is " + alien_0["color"] + ".")
 
 alien_0["color"] = "yellow"
 print("The alien is now " +alien_0["color"] + ".")
+
+alien_0 = {"x_position": 0, "y_position": 25, "speed":"medium"}
+print("Original x_position: " + str(alien_0["x_position"]))
+# Move the alien to the right
+# Determine how far to move the alien based on it's current speed.
+if alien_0["speed"] == "slow":
+    x_increment = 1
+elif alien_0["speed"]  == "medium":
+    x_increment = 2
+else:
+    #This must be a fast alien
+    x_increment = 3
+
+# The new position is the old position plus the increment. 
+alien_0["x_position"] = alien_0["x_position"] + x_increment
+
+print("New x_position: " + str(alien_0["x_position"]))
+
+# Removing Key-Value Pairs
+alien_0 = {"color": "green", "points": 5}
+print(alien_0)
+
+del alien_0["points"]
+print(alien_0)
