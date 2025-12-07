30cf5e8c84e931484575f432fda4a54cf9cf8a1f
llusty1 <lawrencelusty@proton.me>
2025-12-07 11:49:15 -0600

    Commit

--- Chapter 6-Dictionaries/favorite_numbers.py
+++ Chapter 6-Dictionaries/favorite_numbers.py
@@ -4,8 +4,8 @@ favorite_numbers = {"Dak":4,
                     "CeeDee":88,
                     "Javonte":33,
                     "Jake":47,}
-print("Dak Prescott's number is: " + str(favorite_numbers["Dak"]))
-print("George Picken's number is: " + str(favorite_numbers["George"]))
-print("CeeDee Lamb's number is: " + str(favorite_numbers["CeeDee"]))
-print("Javonte William's number is: " +str(favorite_numbers["Javonte"]))
-print("Jake Ferguson's bumer is: " + str(favorite_numbers["Jake"]))
+print("Dak Prescott's number is: "+ str(favorite_numbers["Dak"]))
+print("George Picken's number is: "+ str(favorite_numbers["George"]))
+print("CeeDee Lamb's number is: "+ str(favorite_numbers["CeeDee"]))
+print("Javonte William's number is: "+ str(favorite_numbers["Javonte"]))
+print("Jake Ferguson's bumer is: "+ str(favorite_numbers["Jake"]))
---
+++ Chapter 6-Dictionaries/glossary.py
@@ -0,0 +1,1 @@
+# Here is the exercise for a glossary in the chapter
