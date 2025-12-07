77ce37e661b6fe9b048a7f5a8620c0148cea2022
llusty1 <lawrencelusty@proton.me>
2025-12-07 11:16:20 -0600

    Commit

---
+++ Chapter 6-Dictionaries/person.py
@@ -0,0 +1,7 @@
+# This is the "Person" exercise in the chapter.
+# I will use the year that Johnny Thunders became the guitarist of the New York Dolls
+Johnny = {"first_name": "johnny", "last_name": "thunders", "age": 20, "city": "New York City"}
+print(Johnny["first_name"].title())
+print(Johnny["last_name"].title())
+print(Johnny["age"])
+print(Johnny["city"].title())
