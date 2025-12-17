ef44a41b42ea835479c39dcf2f08ac4b203f55ff
llusty1 <lawrencelusty@proton.me>
2025-12-17 08:20:30 -0600

    Commit

--- Chapter 6-Dictionaries/aliens.py
+++ Chapter 6-Dictionaries/aliens.py
@@ -2,7 +2,7 @@
 alien_0 = {"color": "green", "points": 5}
 alien_1 = {"color": "yellow", "points": 10 }
 alien_2 = {"color": "red", "points": 15}
-#Now let's nest those dictionaries:
+#Now let's nest those dictionaries in a list:
 aliens = [alien_0, alien_1, alien_2]
 # A little for loop to print the dictionaries:
 for alien in aliens:
